6733b921dce1cb37b0d7ab9b87bcda3a21f78217
Jahara Clark <jaharamclark@gmail.com>
2023-06-04 18:17:16 -0700

    Push for code pair

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,22 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./modules/sidebar"
+import "./modules/theme"
+import "./modules/feather"
+
+// Charts
+import "./modules/chartjs"
+
+// Forms
+import "./modules/flatpickr"
+
+// Maps
+import "./modules/vector-maps"
+@import url(css / animate.css);
+@import url(css / animate.min.css);
+@import url(css / loading.css...);
+@import url(css / flaticon.css);
+@import url(css / prettyPhoto.css);
+@import url(css / owl.carousel.css);
+@import url(css / font - awesome.min.css);
